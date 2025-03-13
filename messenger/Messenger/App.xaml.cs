07a4640e7c96795969973b6a5ea0916080ac0aac
Santiago <161547789+santiRojas18@users.noreply.github.com>
2025-03-12 20:18:29 -0500

    Add files via upload

---
+++ messenger/Messenger/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Messenger.Views;
+using Messenger.ViewModels;
+
+namespace Messenger
+{
+    public partial class App : Application
+    {
+
+        public App ()
+        {
+            InitializeComponent();
+
+            MainPage = new Principal();
+
+            
+        }
+
+        protected override void OnStart ()
+        {
+        }
+
+        protected override void OnSleep ()
+        {
+        }
+
+        protected override void OnResume ()
+        {
+        }
+    }
+}
---
+++ messenger/Messenger/Models/mensaje.cs
@@ -0,0 +1,27 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Messenger.Models
+{
+    class Mensaje
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }  
+        public int UsuarioId { get; set; }  
+        public string Texto { get; set; }  
+        public DateTime FechaEnvio { get; set; }  
+        public string ChatId { get; set; }  
+
+        public Mensaje() { }
+
+        public Mensaje(int usuarioId, string texto, string chatId)
+        {
+            UsuarioId = usuarioId;
+            Texto = texto;
+            FechaEnvio = DateTime.Now;
+            ChatId = chatId;
+        }
+    }
+}
